e4282358818aca4b2d1f17cfe0416546ad85ccbb
devel0per-code <faraidevelper@outlook.com>
2026-05-21 11:04:44 +0200

    changes

--- src/main/java/com/farai/taskmanager/controller/TaskController.java
+++ src/main/java/com/farai/taskmanager/controller/TaskController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
@@ -21,6 +22,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 @RequestMapping("/api/tasks")
+@CrossOrigin(origins = "http://localhost:5174/")
 public class TaskController {
 
 
